ef7e63d59fd44c2d3292a61e93607a9f2a25c3f4
Enot3811 <enot3811@gmail.com>
2023-09-27 11:09:06 +0300

    Добавлено разбиение на rgb и pol семплы и отображение классовых меток строками

--- mako_camera/server/infer_yolo_dir.py
+++ mako_camera/server/infer_yolo_dir.py
@@ -10,13 +10,21 @@ import numpy as np
 import cv2
 
 sys.path.append(str(Path(__file__).parents[2]))
-sys.path.append(str(Path(__file__).parents[2] / 'Yolov7'))  # TODO сделать что-нибудь с этим
-from Yolov7.custom.model_utils import load_model, load_sample, inference, draw_bboxes_cv2
-from utils.torch_utils import image_tensor_to_numpy
+# TODO сделать что-нибудь с этим
+sys.path.append(str(Path(__file__).parents[2] / 'Yolov7'))
+from Yolov7.custom.model_utils import (
+    load_model, load_rgb_sample, load_pol_sample,
+    inference, draw_bboxes_cv2, idx2label)
+from utils.torch_utils.torch_functions import image_tensor_to_numpy
 
 
-def main(frames_dir: Path, weights: Union[Path, bool],
-         conf_thresh = 0.1, iou_thresh = 0.2, show_time: bool = False
+def main(
+    frames_dir: Path,
+    polarized: bool,
+    weights: Union[Path, bool],
+    conf_thresh: float = 0.6,
+    iou_thresh: float = 0.2,
+    show_time: bool = False
 ) -> None:
     """Запустить yolo на сканирование папки и обработку всех новых кадров.
 
@@ -24,10 +32,11 @@ def main(frames_dir: Path, weights: Union[Path, bool],
     ----------
     frames_dir : Path
         Папка сканирования.
-    file_ext : str
-        Расширения файлов, которые следует загружать.
+    polarized : bool
+        Поляризационная ли съёмка. Если False, то RGB.
     weights : Union[Path, bool]
-        Путь к pt файлу с весами модели или bool для загрузки официальных весов.
+        Путь к pt файлу с весами модели
+        или bool для загрузки официальных весов.
     conf_thresh : float, optional
         Confidence threshold, by default 0.1
     iou_thresh : float, optional
@@ -56,7 +65,10 @@ def main(frames_dir: Path, weights: Union[Path, bool],
             if show_time:
                 start = time.time()
 
-            image = load_sample(pth)
+            if polarized:
+                image = load_pol_sample(pth)
+            else:
+                image = load_rgb_sample(pth)
             c = image.shape[1]
 
             # Если прочитали pol
@@ -74,8 +86,13 @@ def main(frames_dir: Path, weights: Union[Path, bool],
                 print('Время обработки:', time.time() - start)
 
             image = image_tensor_to_numpy(image)
-            image = draw_bboxes_cv2(image[0], boxes.tolist()[:30],
-                                    class_ids.tolist()[:30], confidences.tolist()[:30])
+
+            labels = list(map(lambda idx: idx2label[idx],
+                              class_ids.tolist()[:30]))
+            image = draw_bboxes_cv2(
+                image[0], boxes.tolist()[:30],
+                labels,
+                confidences.tolist()[:30])
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             
         cv2.imshow('yolo', image)
@@ -84,9 +101,6 @@ def main(frames_dir: Path, weights: Union[Path, bool],
         if k == 27:  # esc
             cv2.destroyAllWindows()
             break
-        # # Save frames
-        # elif k == 13:  # enter
-        #     flag = 1
 
 
 def parse_args() -> argparse.Namespace:
@@ -96,14 +110,25 @@ def parse_args() -> argparse.Namespace:
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    parser.add_argument('--frames_dir', help='Директория для сканирования.', type=Path)
+    parser.add_argument('--frames_dir',
+                        help='Директория для сканирования.', type=Path)
+    parser.add_argument('--polarized',
+                        help='Поляризационная съёмка. Если не указан, то RGB.',
+                        action='store_true')
     parser.add_argument('--weights',
-                        help='Веса модели. Путь к pt файлу или bool для загрузки официальных весов',
+                        help='Веса модели. Путь к pt файлу или '
+                        'bool для загрузки официальных весов',
                         type=str, default='True')
-    parser.add_argument('--conf_thresh', help='Порог уверенности модели.', type=float, default=0.8)
-    parser.add_argument('--iou_thresh', help='Порог перекрытия рамок.', type=float, default=0.2)
-    parser.add_argument('--show_time', help='Показывать время выполнения.', action='store_true')
-
+    parser.add_argument('--conf_thresh',
+                        help='Порог уверенности модели.', type=float,
+                        default=0.6)
+    # TODO проверить, почему ни на что не влияет
+    parser.add_argument('--iou_thresh',
+                        help='Порог перекрытия рамок.', type=float,
+                        default=0.2)
+    parser.add_argument('--show_time',
+                        help='Показывать время выполнения.',
+                        action='store_true')
     args = parser.parse_args()
     return args
 
@@ -111,14 +136,15 @@ def parse_args() -> argparse.Namespace:
 if __name__ == '__main__':
     args = parse_args()
     frames_dir = args.frames_dir
+    polarized = args.polarized
     conf_thresh = args.conf_thresh
     iou_thresh = args.iou_thresh
     show_time = args.show_time
     weights = args.weights
-    if weights == 'True':
+    if weights.lower() == 'true':
         weights = True
-    elif weights == 'False':
+    elif weights.lower() == 'false':
         weights = False
     else:
         weights = Path(weights)
-    main(frames_dir, weights, conf_thresh, iou_thresh, show_time)
+    main(frames_dir, polarized, weights, conf_thresh, iou_thresh, show_time)
