d5c8abb58c18ada90a594c64f306f987b4117417
Enot3811 <enot3811@gmail.com>
2023-09-27 11:14:51 +0300

    Правки документации

--- mako_camera/cameras_utils.py
+++ mako_camera/cameras_utils.py
@@ -44,40 +44,41 @@ def calc_Stocks_param(
 
     Parameters
     ----------
-    ch_0 : float
-        0 angle channel.
-    ch_45 : float
-        45 angle channel.
-    ch_90 : float
-        90 angle channel.
-    ch_135 : float
-        135 angle channel.
+    ch_0 : NDArray
+        0 angle channel in float.
+    ch_45 : NDArray
+        45 angle channel in float.
+    ch_90 : NDArray
+        90 angle channel in float.
+    ch_135 : NDArray
+        135 angle channel in float.
 
     Returns
     -------
-    Tuple[float, float, float]
+    Tuple[NDArray, NDArray, NDArray]
         Return S1, S2, S3 parameters.
     """
-    # s_0 = ch_0 + ch_90 + 1
-    s_0 = (ch_0 + ch_90 + ch_45 + ch_135) / 2
-    s_1 = ch_0 - ch_90
-    s_2 = ch_45 - ch_135
-    s_0[s_0 == 0.0] = 1e-7
-    s_1[s_1 == 0.0] = 1e-7
-    s_2[s_2 == 0.0] = 1e-7
-    return s_0, s_1, s_2
+    # s0 = ch_0 + ch_90 + 1
+    s0 = (ch_0 + ch_90 + ch_45 + ch_135) / 2
+    # s0 = ch_0 + ch_90
+    s1 = ch_0 - ch_90
+    s2 = ch_45 - ch_135
+    s0[s0 == 0.0] = 1e-7
+    s1[s1 == 0.0] = 1e-7
+    s2[s2 == 0.0] = 1e-7
+    return s0, s1, s2
 
 
-def calc_AoLP(s_1: NDArray, s_2: NDArray) -> NDArray:
+def calc_AoLP(s1: NDArray, s2: NDArray) -> NDArray:
     """Calculate angle of polarization.
 
-    Normal values range is from -1.57 to 1.57.
+    Normal values range is from -1.57pi to 1.57pi.
 
     Parameters
     ----------
-    s_1 : NDArray
+    s1 : NDArray
         S1 stoke parameter. Expected float array in range (-1, 1).
-    s_2 : NDArray
+    s2 : NDArray
         S2 stoke parameter. Expected float array in range (-1, 1).
 
     Returns
@@ -85,7 +86,7 @@ def calc_AoLP(s_1: NDArray, s_2: NDArray) -> NDArray:
     NDArray
         Pixelwice angle of polarization in radians. Range is (0, 2*pi)
     """
-    AoLP = 0.5 * np.arctan2(s_2, s_1)
+    AoLP = 0.5 * np.arctan2(s2, s1)
     return AoLP
 
 
@@ -110,14 +111,12 @@ def pol_intensity(s1: NDArray, s2: NDArray) -> NDArray:
     # return np.sqrt(np.square(s1) + np.square(s2))
 
 
-def calc_DoLP(s_0: NDArray, pol_int: NDArray) -> NDArray:
+def calc_DoLP(s0: NDArray, pol_int: NDArray) -> NDArray:
     """Calculate degree of linear polarization.
-
-    Normal values range is from 0 to 1.
 
     Parameters
     ----------
-    s_0 : NDArray
+    s0 : NDArray
         S0 stock parameter.
     pol_int : NDArray
         Polarization intensity.
@@ -127,7 +126,7 @@ def calc_DoLP(s_0: NDArray, pol_int: NDArray) -> NDArray:
     NDArray
         Degree of linear polarization.
     """
-    DoLP = pol_int / s_0
+    DoLP = pol_int / s0
     return DoLP
 
 
