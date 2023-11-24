f2bc167c3db6c2c840e009aac60c1c93e1ea3246
Enot3811 <enot3811@gmail.com>
2023-11-24 13:28:09 +0300

    Обновление скрипта проверки датасета

--- Yolov7/custom/datasets/test/test_tank_detection_dataset.py
+++ Yolov7/custom/datasets/test/test_tank_detection_dataset.py
@@ -1,18 +1,18 @@
-"""Скрипт для итерации по датасетам перед обучениям и просмотра семплов.
+"""Тест для модуля `tank_detection_additional`.
 
-Позволяет покрутить параметры датасетов и посмотреть на результаты аугментаций.
+Требуется конфиг обучения, где планируется использовать данный модуль.
 """
-
 
 import sys
 from pathlib import Path
 import argparse
+import json
 
 from torchvision.ops import box_convert
 import cv2
 import albumentations as A
 
-sys.path.append(str(Path(__file__).parents[3]))
+sys.path.append(str(Path(__file__).parents[4]))
 from Yolov7.yolov7.dataset import (
     Yolov7Dataset, create_yolov7_transforms)
 from Yolov7.yolov7.mosaic import (
@@ -22,38 +22,43 @@ from utils.torch_utils.torch_functions import (
     draw_bounding_boxes, image_tensor_to_numpy)
 
 
-def main(**kwargs):
-    # Parse args
-    dset_dir = kwargs['dset_dir']
-    dset_type = kwargs['dset_type']
+def main(config_pth: str, dset_type: str):
+    # Read config
+    with open(config_pth, 'r') as f:
+        config_str = f.read()
+    config = json.loads(config_str)
 
-    # Get dataset parameters
-    input_size = 640
-    mosaic_prob = 1.0
-    mixup_prob = 0.15
-    training = dset_type == 'train'
-    
+    dset_pth = Path(config['dataset']) / dset_type
+
     # Get transforms
     post_mosaic_transforms = create_post_mosaic_transform(
-        input_size, input_size)
+        config['input_size'], config['input_size'])
     resize_to_show = A.Compose(
-        [A.LongestMaxSize(input_size)],
+        [A.LongestMaxSize(config['input_size'])],
         bbox_params=A.BboxParams(format='pascal_voc',
                                  label_fields=['classes']))
     # post_mosaic_transforms = None
+    training_transforms = [
+        A.RandomCropFromBorders(crop_left=0.4, crop_right=0.4,
+                                crop_top=0.4, crop_bottom=0.4),
+        A.HorizontalFlip()
+    ]
     yolo_transforms = create_yolov7_transforms(
-        (input_size, input_size), training)
+        (config['input_size'], config['input_size']), dset_type,
+        training_transforms=training_transforms)
 
     # Get dataset
-    obj_dset = TankDetectionDataset(dset_dir)
+    obj_dset = TankDetectionDataset(
+        dset_pth, name2index=config['cls_to_id'],
+        polarization=config['polarization'])
     index_to_cls = obj_dset.index_to_class
 
     mosaic_mixup_dset = MosaicMixupDataset(
         obj_dset,
-        apply_mosaic_probability=mosaic_prob,
-        apply_mixup_probability=mixup_prob,
+        apply_mosaic_probability=config['mosaic_prob'],
+        apply_mixup_probability=config['mixup_prob'],
         post_mosaic_transforms=post_mosaic_transforms)
-    if not training:
+    if dset_type != 'train':
         mosaic_mixup_dset.disable()
     
     yolo_dset = Yolov7Dataset(mosaic_mixup_dset, yolo_transforms)
@@ -80,9 +85,12 @@ def main(**kwargs):
         yolo_bboxes = box_convert(yolo_bboxes, 'cxcywh', 'xyxy')
         ls_yolo_bboxes = yolo_bboxes.tolist()
 
-        str_orig_cls = list(map(lambda idx: index_to_cls[idx], orig_cls))
-        str_mos_cls = list(map(lambda idx: index_to_cls[idx], mos_cls))
-        str_ls_yolo_cls = list(map(lambda idx: index_to_cls[idx], ls_yolo_cls))
+        str_orig_cls = list(map(lambda idx: index_to_cls[int(idx)],
+                                orig_cls))
+        str_mos_cls = list(map(lambda idx: index_to_cls[int(idx)],
+                               mos_cls))
+        str_ls_yolo_cls = list(map(lambda idx: index_to_cls[int(idx)],
+                                   ls_yolo_cls))
 
         resized = resize_to_show(
             image=orig_img, bboxes=orig_bboxes, classes=orig_cls)
@@ -104,12 +112,15 @@ def main(**kwargs):
               'Augmented base shape:', orig_img.shape)
         print('Mosaic original shape:', mos_shape,
               'Augmented mosaic shape:', mos_img.shape)
-        print('Mosaic original shape:', yolo_shape,
-              'Augmented mosaic shape:', yolo_img.shape, '\n')
+        print('Yolo original shape:', yolo_shape,
+              'Augmented yolo shape:', yolo_img.shape, '\n')
 
-        cv2.imshow('orig', cv2.cvtColor(orig_img_bboxes, cv2.COLOR_RGB2BGR))
-        cv2.imshow('mos_mix', cv2.cvtColor(mos_img_bboxes, cv2.COLOR_RGB2BGR))
-        cv2.imshow('yolo', cv2.cvtColor(yolo_img_bboxes, cv2.COLOR_RGB2BGR))
+        cv2.imshow('orig', cv2.cvtColor(orig_img_bboxes,
+                   cv2.COLOR_RGB2BGR))
+        cv2.imshow('mos_mix', cv2.cvtColor(mos_img_bboxes,
+                   cv2.COLOR_RGB2BGR))
+        cv2.imshow('yolo', cv2.cvtColor(yolo_img_bboxes,
+                   cv2.COLOR_RGB2BGR))
         key = cv2.waitKey(0)
         if key == 27:
             break
@@ -123,18 +134,21 @@ def parse_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
-        'dset_dir', type=Path,
-        help='Путь к директории с датасетом. Подразумевается CVAT формат.')
+        'config_pth', type=Path,
+        help='Путь к конфигу обучения.')
     parser.add_argument(
         '--dset_type', type=str, default='train', choices=['train', 'val'],
         help='Для "train" применяются аугментации, а для "val" нет.')
 
     args = parser.parse_args()
+
+    if not args.config_pth.exists():
+        raise FileExistsError('Config file does not exist.')
     return args
 
 
 if __name__ == "__main__":
     args = parse_args()
-    dset_dir = args.dset_dir
+    config_pth = args.config_pth
     dset_type = args.dset_type
-    main(dset_dir=dset_dir, dset_type=dset_type)
+    main(config_pth=config_pth, dset_type=dset_type)
