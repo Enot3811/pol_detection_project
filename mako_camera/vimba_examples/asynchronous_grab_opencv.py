3d4181fc26a93684b740629f7273eb2c18a10b18
Enot3811 <enot3811@gmail.com>
2024-02-06 15:02:34 +0300

    Изменена директория импортов для vimba async grab cv2

--- mako_camera/vimba_examples/asynchronous_grab_opencv.py
+++ mako_camera/vimba_examples/asynchronous_grab_opencv.py
@@ -32,7 +32,7 @@ from pathlib import Path
 
 from vimba import *
 
-sys.path.append(str(Path(__file__).parents[1]))
+sys.path.append(str(Path(__file__).parents[2]))
 from utils.image_utils.image_functions import resize_image
 
 
