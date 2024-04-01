ddc024918f7c4d68190b40a13dad371fdd4b38e2
Enot3811 <enot3811@gmail.com>
2024-04-01 13:07:02 +0300

    Update init in torch utils datasets

--- utils/torch_utils/datasets/__init__.py
+++ utils/torch_utils/datasets/__init__.py
@@ -1,4 +1,8 @@
 """The module contains torch datasets classes."""
 
-from utils.torch_utils.datasets.cvat_object_detections_dataset import (  # noqa
-    TorchCvatObjectDetectionDataset)
+from utils.torch_utils.datasets.cvat_object_detection_dataset import (  # noqa
+    CvatObjectDetectionDataset)
+from utils.torch_utils.datasets.abstract_torch_dataset import (  # noqa
+    AbstractTorchDataset)
+from utils.torch_utils.datasets.abstract_classification_dataset import (  # noqa
+    AbstractClassificationDataset)
