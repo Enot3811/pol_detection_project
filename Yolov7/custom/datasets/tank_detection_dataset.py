9f3971c6e69b09dffb7475c878f7e2fe6471138b
Enot3811 <enot3811@gmail.com>
2024-04-08 17:29:37 +0300

    Update tank detection dataset with new hierarchy

--- Yolov7/custom/datasets/tank_detection_dataset.py
+++ Yolov7/custom/datasets/tank_detection_dataset.py
@@ -2,107 +2,110 @@
 
 
 from pathlib import Path
-from typing import Dict, Union, Callable, Tuple, List
+from typing import Dict, Union, Callable, Any, Tuple
 
 import numpy as np
 from numpy.typing import NDArray
+import torch
 
-from utils.torch_utils.datasets import TorchCvatObjectDetectionDataset
+from utils.torch_utils.datasets import CvatObjectDetectionDataset
 from mako_camera.cameras_utils import split_raw_pol
 
 
-class TankDetectionDataset(TorchCvatObjectDetectionDataset):
-    """Tanks object detection dataset for yolo project.
+class PolarizationObjectDetectionDataset(CvatObjectDetectionDataset):
+    """Polarization object detection dataset in CVAT format for yolo project.
 
-    This dataset is used as a connection between base torch object detection
-    class and yolo mosaic dataset class.
+    Samples format of this dataset is compatible to use in yolo mosaic dataset.
     It's required that this dataset returns an image, a bounding boxes,
     classes ids, an image id, original hw of the image.
+
+    Parameters
+    ----------
+    dset_pth : Union[Path, str]
+        Path to CVAT dataset directory.
+    device : torch.device, optional
+        Device for dataset samples. By default is `torch.device('cpu')`.
+    transforms : Callable, optional
+        Transforms that performs on sample.
+        Required that it has `albumentations.Compose` like structure.
+        By default is `None`.
+    class_to_index : Dict[str, int], optional
+        User-defined class to index mapping. It required that this dict
+        contains all classes represented in the dataset. By default is `None`.
+    polarization : bool, optional
+        Is this a polarization dataset or RGB. By default is False (RGB).
     """
+    
     def __init__(
         self,
-        cvat_dset_dir: Union[str, Path],
-        name2index: Dict[str, int] = None,
+        dset_pth: Union[Path, str],
+        device: torch.device = torch.device('cpu'),
         transforms: Callable = None,
+        class_to_index: Dict[str, int] = None,
         polarization: bool = False
-    ):
-        """Initialize TankDetectionDataset object.
-
-        Parameters
-        ----------
-        cvat_dset_dir : Union[str, Path]
-            A path to a cvat dataset directory.
-        name2index : Dict[str, int], optional
-            Name to index dict converter.
-            If not provided then will be generated automatically.
-        transforms : Callable, optional
-            Dataset transforms.
-            It's expected that "Albumentations" lib will be used.
-            By default is None.
-        pol_sample : bool, optional
-            Is this a polarization dataset or RGB. By default is False (RGB).
-        """
-        super().__init__(cvat_dset_dir, name2index, transforms)
+    ) -> None:
+        super().__init__(dset_pth, device, transforms, class_to_index)
+        # Save polarization mode
         self.polarization = polarization
+        # Create img to id maps (required by mosaic and YOLOv7 datasets)
         self.img_name_to_id = {
             cvat_sample['name']: i
-            for i, cvat_sample in enumerate(self.cvat_dset)}
+            for i, cvat_sample in enumerate(self.samples)}
         self.img_id_to_name = {
             id: name
             for name, id, in self.img_name_to_id.items()}
 
-    def __getitem__(
-        self, idx: int
-    ) -> Tuple[NDArray, NDArray, NDArray, int, Tuple[int, int]]:
-        """
-        Get a sample from dataset by index.
+    def get_sample(self, index: int) -> Dict[str, Any]:
+        """Get CVAT tank detection sample.
+
+        Sample represented as a dict that contains "image" `ndarray`,
+        "bboxes" `list[list[float]]`, "classes" `list[int]`, "img_pth" `Path`
+        and "shape" `tuple[int, int]`.
+
+        If `self.polarization` is `True` then image will be parsed from
+        `(h, w)` shape to `(h / 2, w / 2, 4)`.
 
         Parameters
         ----------
-        idx : int
-            An index of sample.
+        index : int
+            Index of sample.
 
         Returns
         -------
-        Tuple[np.ndarray, np.ndarray, np.ndarray, int, Tuple[int, int]]
-            An image, a bounding box, classes ids, an image id,
-            original hw of the image.
+        Dict[str, Any]
+            CVAT tank detection sample by index.
         """
-        # Polarization read
+        sample = super().get_sample(index)
+        sample['img_id'] = self.img_name_to_id[sample['img_pth'].name]
+        # Process if polarization
         if self.polarization:
-            sample = self.cvat_dset[idx]
-            img_name = sample['name']
-            classes = sample['labels']
-            classes = list(map(lambda label: float(self.class_to_index[label]),
-                               classes))
-            bboxes = sample['bboxes']
-            shape = sample['shape']
-            image = np.load(self.image_dir / img_name)
-            image = split_raw_pol(image)
-            shape = image.shape[:2]
-            if self.transforms is not None:
-                self.apply_transforms(image, bboxes, classes)
-                transformed = self.transforms(
-                    image=image, bboxes=bboxes, labels=classes
-                )
-                image = transformed['image']
-                bboxes = transformed['bboxes']
-                classes = transformed['classes']
-        # RGB read
-        else:
-            image, bboxes, classes, img_name, shape = super().__getitem__(idx)
-        bboxes = np.array(bboxes)
-        classes = np.array(classes)
-        img_id = self.img_name_to_id[img_name]
-        return image, bboxes, classes, img_id, shape
+            sample['image'] = split_raw_pol(sample['image'])
+        return sample
+    
+    def postprocess_sample(
+        self, sample: Dict[str, Any]
+    ) -> Tuple[NDArray, NDArray, NDArray, int, Tuple[int, int]]:
+        """Process the sample to make it suitable for `MosaicMixupDataset`.
 
-    def apply_transforms(
-        self, image: NDArray, bboxes: List[List[float]], classes: List[float]
-    ) -> Tuple[NDArray, List[List[float]], List[float]]:
-        # Labels argument's name was changed
-        transformed = self.transforms(
-            image=image, bboxes=bboxes, labels=classes)
-        image = transformed['image']
-        bboxes = transformed['bboxes']
-        classes = transformed['labels']
-        return image, bboxes, classes
+        Unpack CVAT dictionary sample, convert bboxes and classes to `NDArray`
+        and pack it to a `tuple`.
+
+        Parameters
+        ----------
+        sample : Dict[str, Any]
+            CVAT sample with additional "img_id" `int`.
+
+        Returns
+        -------
+        Tuple[NDArray, NDArray, NDArray, int, Tuple[int, int]]
+            Image, bboxes, classes, image id, original shape.
+        """
+        # Convert bboxes and classes from lists to arrays as required
+        sample['bboxes'] = np.array(sample['bboxes'])
+        sample['classes'] = np.array(sample['classes'])
+        image = sample['image']
+        bboxes = sample['bboxes']
+        classes = sample['classes']
+        shape = sample['shape']
+        img_id = sample['img_id']
+        return image, bboxes, classes, img_id, shape
