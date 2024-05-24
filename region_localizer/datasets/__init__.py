408c4561e234eb5521bd3902c2290dbe28f97cfb
Enot3811 <enot3811@gmail.com>
2024-05-24 13:27:51 +0300

    Update region datasets init

--- region_localizer/datasets/__init__.py
+++ region_localizer/datasets/__init__.py
@@ -1,2 +1,3 @@
 from region_localizer.datasets.region_dataset import RegionDataset  # noqa
 from region_localizer.datasets.region_dataset_v2 import RegionDatasetV2  # noqa
+from region_localizer.datasets.region_dataset_v2_dif import RegionDatasetV2Dif  # noqa
