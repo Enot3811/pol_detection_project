f0bfb347db25332280e3748b32dc3293ecb98239
Enot3811 <enot3811@gmail.com>
2024-06-03 15:33:35 +0300

    Update region lacalizer inference script

--- region_localizer/inference.py
+++ region_localizer/inference.py
@@ -20,8 +20,8 @@ given_dir
 где в regions лежат изображения регионов, а в pieces изображения фрагментов
 из соответствующих регионов и соответствующие текстовые файлы
 с xyxy координатами фрагментов относительно региона.
-Если директория pieces отсутствует, то фрагменты будут производиться
-автоматически из изображений регионов.
+Если директория pieces отсутствует, то фрагменты автоматически создадутся
+из изображений регионов с помощью "generate_pieces" скрипта.
 Файлы txt с координатами рамок используются для отображения рамки фрагмента
 на изображении региона. Если файл отсутствует, то и отображение производиться
 не будет.
@@ -33,15 +33,16 @@ from pathlib import Path
 import json
 import sys
 
+import numpy as np
 import torch
 from torchmetrics.detection import IntersectionOverUnion
 
 sys.path.append(str(Path(__file__).parents[1]))
 from region_localizer.models import RetinaRegionLocalizer, ModifiedRetinaV2
-from utils.image_utils.image_functions import (
-    collect_images_paths, read_image, resize_image, show_images_cv2)
+from utils.data_utils.data_functions import resize_image, show_images_cv2
 from utils.torch_utils.torch_functions import (
     image_numpy_to_tensor, draw_bounding_boxes)
+from utils.data_utils.data_functions import collect_paths, read_image
 from region_localizer.scripts.generate_pieces import main as generate_pieces
 
 
@@ -81,6 +82,10 @@ def main(
     input_size = config['val_dataset_params']['result_size']
     input_size = ((input_size,) * 2 if isinstance(input_size, int)
                   else input_size)
+    if 'map_grayscale' in config['val_dataset_params']:
+        map_grayscale = config['val_dataset_params']['map_grayscale']
+    else:
+        map_grayscale = False
 
     # Load model
     if config['architecture'] == 'modified_retina':
@@ -91,10 +96,13 @@ def main(
             num_classes=config['num_classes'])
     elif config['architecture'] == 'modified_retina_v2':
         model = ModifiedRetinaV2.create_modified_retina(
+            input_channels=config['input_channels'],
             min_size=config['train_dataset_params']['result_size'][0],
             max_size=config['train_dataset_params']['result_size'][0],
             pretrained=config['pretrained'],
-            num_classes=config['num_classes'])
+            num_classes=config['num_classes'],
+            image_mean=config['image_mean'],
+            image_std=config['image_std'])
     else:
         raise ValueError(f'Unrecognized architecture {config["architecture"]}')
 
@@ -105,12 +113,12 @@ def main(
     model.eval()
 
     # Iterate over regions
-    regions_pths = collect_images_paths(image_dir / 'regions')
+    regions_pths = collect_paths(
+        image_dir / 'regions', file_extensions=('jpg', 'npy'))
     regions_pths.sort()
     for region_pth in regions_pths:
-        region_img = read_image(region_pth)
-        region_name = region_pth.name.split('.')[0]
-        region_pieces_dir = pieces_dir / region_name
+        region_img = read_image(region_pth, map_grayscale)
+        region_pieces_dir = pieces_dir / region_pth.stem
 
         # Prepare map image to pass to the model
         orig_size = region_img.shape[:2]
@@ -121,7 +129,8 @@ def main(
         # Iterate over region's fragments
         pieces_bboxes_pths = list(region_pieces_dir.glob('*.txt'))
         pieces_bboxes_pths.sort()
-        pieces_pths = list(region_pieces_dir.glob('*.jpg'))
+        pieces_pths = collect_paths(
+            region_pieces_dir, file_extensions=('jpg', 'npy'))
         pieces_pths.sort()
         for bbox_pth, piece_pth in zip(pieces_bboxes_pths, pieces_pths):
             orig_piece_img = read_image(piece_pth)
@@ -142,12 +151,26 @@ def main(
             _, predictions = model(input_img)
 
             # Prepare results
+
+            bbox_region = region_img
+            # Add 3-ch for colorful bboxes
+            if bbox_region.shape[-1] == 1:
+                bbox_region = np.repeat(bbox_region, 3, axis=2)
+
             pred_bbox = predictions[0]['boxes'][0].tolist()
             bbox_region = draw_bounding_boxes(
-                region_img, [bbox], color=(0, 255, 0))
+                bbox_region, [bbox], color=(0, 255, 0))
             bbox_region = draw_bounding_boxes(
                 bbox_region, [pred_bbox], color=(255, 0, 0))
             
+            # Resize for screen size
+            if bbox_region.shape[0] > 1000:
+                bbox_region = resize_image(bbox_region, (1000, 1000))
+            if (orig_piece_img.shape[0] > 1000 and
+                isinstance(config['train_dataset_params']['min_crop_size'],
+                           int)):  # Проверка на квадрат
+                orig_piece_img = resize_image(orig_piece_img, (1000, 1000))
+
             key = show_images_cv2(
                 [bbox_region, orig_piece_img],
                 ['Red - predict, green - gt', 'Input'],
